d294a8585b7fa9812231f47a55f37bbb0a97eb9d
tamur aziz <tamur86@gmail.com>
2018-03-05 08:40:55 +0100

    Upgift2 JS

---
+++ bnbdata.js
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Project AirBNB  </title>
+
+		<style>
+
+			body { Background-color: DarkRed;
+				 }
+
+			#myDiv1 {
+				float:Right ;
+				border-style: solid;
+				border-color: Green;
+				width:410px;
+				height:280px;
+				margin-right:2px;
+				margin-bottom:2px;
+					}
+
+			#myDiv2 {
+				float: Right;
+				border-style: solid;
+				border-color: Green;
+				width:410px;
+				height:280px;
+				margin-right:2px;
+				margin-bottom:2px;
+					}
+
+			#myDiv3 {
+				float: Right;
+				border-style: solid;
+				border-color: Green;
+				width:405px;
+				height:280px;
+				margin-right:2px;
+				margin-bottom:2px;
+					}
+
+			#myDiv4 {
+				float: Right;
+				border-style: solid;
+				border-color: Green;
+				width:615px;
+				height:350px;
+				margin-right:2px;
+				margin-bottom:2px;
+					}
+
+			#myDiv5 {
+				float: Right;
+				border-style: solid;
+				border-color: Green;
+				width:615px;
+				height:350px;
+				margin-right:2px;
+				margin-bottom:2px;
+					}
+		</style>
+</head>
+		<body>
+				<div id="myDiv5"></div>
+				<div id="myDiv4"></div>
+				<div id="myDiv3"></div>
+				<div id="myDiv2"></div>
+				<div id="myDiv1"></div>
+		</body>
+
+	<script src="./plotly-latest.min.js"></script>
+	<script src="./bnbdata.js"></script>
+</html>
